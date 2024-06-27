45330d68a6134473927601fc43ce5a290bd89e03
Vedantlearn <112773665+Vedantlearn@users.noreply.github.com>
2024-06-27 12:24:11 +0530

    Update MyWindow.java

--- digital clock/src/com/practice/MyWindow.java
+++ digital clock/src/com/practice/MyWindow.java
@@ -2,7 +2,7 @@ package com.practice;
 import javax.swing.*;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.ActionListener;   
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.awt.*;
